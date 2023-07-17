a47e73b5b1011e599ba7a1a587a56dcecbadde09
GordonOtieno <otienogordon95@gmail.com>
2023-07-17 10:54:56 +0300

    Create system pages folder

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div>ContactUs Page
+      <button className='btn btn-primary'>welcome</button>
+    </div>
+  )
+}
+
+export default ContactUs
