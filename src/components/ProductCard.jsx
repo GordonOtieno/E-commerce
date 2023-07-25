1af8e3e36c0319c10dbf296b44030b9c7dace70f
GordonOtieno <otienogordon95@gmail.com>
2023-07-25 18:29:30 +0300

    Create and Update ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactStars from 'react-rating-stars-component'
+
+const ProductCard = () => {
+    const raingChanged = (newrating) =>{
+      console.log(newrating);
+    }
+  return (
+    <div className='col-3'>
+      <div className="product-card">
+        <div className="product-image">
+            <img src="images/watch.jpg" alt="product image" />
+        </div>
+        <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">Kids Headphones bulk 10 pack muli colored for students
+            </h5>
+            <p className="price">Ksh. 5000</p>
+            <ReactStars 
+            count={5}
+            onChange= {raingChanged}
+            size={24}
+            activeColor='#ffd700'
+            />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
