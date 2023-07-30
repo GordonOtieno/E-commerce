dd54f300d3711dbe5f785b5f98623ca864a1dd76
GordonOtieno <otienogordon95@gmail.com>
2023-07-30 23:06:35 +0300

    Update Home component file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -201,6 +201,21 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="featured-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h2 className='p-3'>Featured Collection</h2>
+              <div className="blogs d-flex gap-4">
+                 <ProductCard />
+                 <ProductCard />
+                 <ProductCard />
+                 <ProductCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="marquee-wrapper py-5">
         <div className="container-xxl">
           <div className="row">
@@ -238,21 +253,6 @@ const Home = () => {
           </div>
         </div>
 
-      </section>
-      <section className="blog-wrapper py-5">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <h2 className='p-3'>Featured Collection</h2>
-              <div className="blogs d-flex gap-4">
-                 <ProductCard />
-                 <ProductCard />
-                 <ProductCard />
-                 <ProductCard />
-              </div>
-            </div>
-          </div>
-        </div>
       </section>
       <section className="blog-wrapper py-5">
         <div className="container-xxl">
